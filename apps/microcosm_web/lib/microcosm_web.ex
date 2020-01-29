9accdc99179a5bffe86e3281f39f37af56a0ef7c
Lance Petersen <lance@goskip.com>
2020-01-29 09:42:28 -0700

    heroku stuff

--- apps/microcosm_web/lib/microcosm_web.ex
+++ apps/microcosm_web/lib/microcosm_web.ex
@@ -42,7 +42,7 @@ defmodule MicrocosmWeb do
       import MicrocosmWeb.ErrorHelpers
       import MicrocosmWeb.Gettext
       alias MicrocosmWeb.Router.Helpers, as: Routes
-      import Phoenix.LiveView, only: [live_render: 2, live_render: 3]
+      # import Phoenix.LiveView, only: [live_render: 2, live_render: 3]
     end
   end
 
--- apps/microcosm_web/test/microcosm_web/controllers/page_controller_test.exs
+++ apps/microcosm_web/test/microcosm_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule MicrocosmWeb.PageControllerTest do
   use MicrocosmWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
+    conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
